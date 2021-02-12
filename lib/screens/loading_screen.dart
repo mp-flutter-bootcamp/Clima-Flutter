7f25ecd15d31691e8057c104b300d63b4e4162c8
micha <micha@home.net>
2021-02-12 16:57:01 +0100

    Load weather data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,11 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import '../services/networking.dart';
+import 'location_screen.dart';
+
+const String apiKey = '91790f98ff0e110d4b1ab2dec2b83788';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,21 +13,45 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
 
-    print("Lat: ${location.latitude}, Lon: ${location.longitude}");
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LocationScreen(),
+        ));
+    // double temperature = decodedResponse['main']['temp'];
+    // int condition = decodedResponse['weather'][0]['id'];
+    // String cityName = decodedResponse['name'];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100,
+        ),
+      ),
+    );
   }
 }
